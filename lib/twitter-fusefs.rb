a49c212c30bf47ee195a0da8e2cf2f2d999a5f5e
chmeee <chmeee@gmail.com>
2009-08-16 23:07:40 +0200

    Added class helper config_store to read $HOME/.twitter config Fixed FuseDir class typo

--- lib/twitter-fusefs.rb
+++ lib/twitter-fusefs.rb
@@ -1,8 +1,10 @@
 require 'twitter'
 require 'fusefs'
+require File.join(File.dirname(__FILE__), '..', 'helpers', 'config_store')
+
 include FuseFS
 
-class TwitterFuseFS < FuseFS::FuseDIR
+class TwitterFuseFS < FuseFS::FuseDir
   def initialize
     config = ConfigStore.new("#{ENV['HOME']}/.twitter")
     httpauth = Twitter::HTTPAuth.new(config['email'], config['password'])
